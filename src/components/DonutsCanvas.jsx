20b59d320e364c02e8e2824cb8d0f1e6970c5af4
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-06 22:46:52 +0300

    format

--- src/components/DonutsCanvas.jsx
+++ src/components/DonutsCanvas.jsx
@@ -8,15 +8,12 @@ export default function DonutsCanvas({ speed = 1, count = 80, depth = 80, easing
   return (
     // No need for antialias (faster), dpr clamps the resolution to 1.5 (also faster than full resolution)
     <Canvas gl={{ antialias: false }} camera={{ position: [0, 0, 10], fov: 20, near: 0.01, far: depth + 15 }} dpr={[1, 1.5]}>
-      {/* <color args={['#FBE195']} attach='background' /> */}
-
       {/* Lights */}
-      {/* <ambientLight intensity={0.2} /> */}
       <spotLight position={[10, 20, 10]} penumbra={1} intensity={3} color='orange' />
 
       {/* Objects */}
       {Array.from({ length: count }, (_, i) => (
-         <Donut key={i} index={i} pZ={Math.round(easing(i / count) * depth)} speed={speed} /> 
+        <Donut key={i} index={i} pZ={Math.round(easing(i / count) * depth)} speed={speed} />
       ))}
 
       <Environment files={'venice_sunset_1k.hdr'} />

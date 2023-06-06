29b515a89c4cae39b931d2c1d34e28655b7acdde
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-06 20:43:49 +0300

    add init overlay

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -1,3 +1,94 @@
+import styled from 'styled-components';
+
 export default function Overlay() {
-  return <div>Overlay</div>;
+  return (
+    <Container>
+      <Title>
+        <h1>DONUT <br />DELIRIUM —</h1>
+        <p>Sprinkle your day with sugary delight. Donut dreams come true! ⎯</p>
+      </Title>
+      <Hamburger><div /><div /><div /></Hamburger>
+      <Description>
+        Indulge in the hole-y goodness<br />
+        Where every bite is pure delight
+      </Description>
+      <Poem>
+        Doughy rings of joy,<br />
+        Sprinkles dancing with delight,<br />
+        Donuts are a snack,<br />
+        That'll keep you up all night.<br /><br />
+              
+        Dip them in chocolate,<br />
+        Or glaze them with sweet cream,<br />
+        Donuts are a treat,<br />
+        A sugary, dreamy theme.<br />
+      </Poem>
+      <Sketch>{/* <DoughnutSketch /> */}</Sketch>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  font-family: 'Inter';
+  font-size: 16px;
+  & h1 {
+    padding: 0;
+    margin: 0 0 0.05em 0;
+    font-family: 'Ayer Poster', serif;
+    font-weight: 400;
+    font-size: min(18vw, 14em);
+    line-height: 0.85em;
+  }
+`;
+
+const Title = styled.div`
+  position: absolute;
+  top: 5vw;
+  left: 5vw;
+`;
+
+const Hamburger = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  top: 5vw;
+  right: 5vw;
+  & > div {
+    position: relative;
+    width: 24px;
+    height: 2px;
+    background: #252525;
+    margin-bottom: 6px;
+  }
+`;
+
+const Description = styled.div`
+  position: absolute;
+  bottom: 5vw;
+  left: 5vw;
+  width: 30ch;
+  max-width: 40%;
+`;
+
+const Poem = styled.div`
+  position: absolute;
+  bottom: 5vw;
+  right: 5vw;
+  width: 35ch;
+  max-width: 40%;
+  line-height: 1em;
+  letter-spacing: -0.01em;
+  text-align: right;
+`;
+
+const Sketch = styled.svg`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate3d(-50%, -50%, 0) rotate(0deg);
+  width: 75%;
+  height: 75%;
+  & g.skin {
+    cursor: pointer;
+  }
+`;

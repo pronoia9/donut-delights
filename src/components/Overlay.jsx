eef3cb7a64208c62a27ddc94f154af8daaa07588
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-07 21:24:16 +0300

    lower line height

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -44,7 +44,7 @@ export default function Overlay({ theme, setTheme }) {
 const Container = styled.div`
   font-family: 'Aaargh', sans-serif;
   font-size: 1rem;
-  line-height: 1.5rem;
+  line-height: 1.25rem;
 
   @media (max-width: 500px) {
     font-size: 0.85rem;

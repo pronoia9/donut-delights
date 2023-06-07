0ec066212d07d5468f3709ef9a880f4d20607391
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-07 21:20:04 +0300

    add opposite color for font2

--- src/styles/Themes.js
+++ src/styles/Themes.js
@@ -1,11 +1,13 @@
 export const darkTheme = {
   font: '#000',
+  font2: '#fff',
   background: '#000',
   background2: '#444',
-}
+};
 
 export const lightTheme = {
   font: '#fff',
+  font2: '#000',
   background: '#fff',
   background2: '#eee',
 };

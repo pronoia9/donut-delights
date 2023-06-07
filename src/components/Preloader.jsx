19f33af8ff747fa6183438ba4ba29f962d5d7957
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-07 20:40:01 +0300

    init preloader

---
+++ src/components/Preloader.jsx
@@ -0,0 +1,6 @@
+const Preloader = () => {
+  return (
+    <div>Preloader</div>
+  )
+}
+export default Preloader
--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,7 @@ import Donut from './Donut';
 import DonutsCanvas from './DonutsCanvas';
 import DonutSketch from './DonutSketch';
 import Overlay from './Overlay';
+import Preloader from './Preloader';
+import ThemeButton from './ThemeButton';
 
-export { Donut, DonutsCanvas, DonutSketch, Background, Overlay };
+export { Donut, DonutsCanvas, DonutSketch, Background, Overlay, Preloader, ThemeButton };

25df29e79694e2625d2eba87465479e7969aeb91
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-10 02:08:07 +0300

    update shaders

--- src/components/DonutsBackground.jsx
+++ src/components/DonutsBackground.jsx
@@ -4,8 +4,9 @@ import { extend, useFrame, useThree } from '@react-three/fiber';
 import { shaderMaterial } from '@react-three/drei';
 
 import vertexShader from '../shaders/vertexShader.glsl';
-import fragmentShader from '../shaders/fragmentShader.glsl';
-import { darkTheme } from '../styles/Themes';
+import fragmentShader1 from '../shaders/fragmentShader1.glsl';
+import fragmentShader2 from '../shaders/fragmentShader2.glsl';
+import { isDarkTheme } from '../utils/utils';
 
 const darkColors = ['', '#225ee1', '#28d7bf', '#ac53cf', '#e7a39c']; // dark
 const mediumColors = ['', '#4d8be9', '#6ce4d6', '#d485e4', '#f2c5ba']; // medium
@@ -17,13 +18,13 @@ extend({
     {
       uResolution: [window.innerWidth * 0.8, window.innerHeight * 0.8],
       uTime: 0,
-      uColor1: new Color(lightColors[1]),
-      uColor2: new Color(lightColors[2]),
-      uColor3: new Color(lightColors[3]),
-      uColor4: new Color(lightColors[4]),
+      uColor1: new Color(colorfulColors[1]),
+      uColor2: new Color(colorfulColors[2]),
+      uColor3: new Color(colorfulColors[3]),
+      uColor4: new Color(colorfulColors[4]),
     },
     vertexShader,
-    fragmentShader
+    fragmentShader1
   ),
 });
 
@@ -32,20 +33,22 @@ extend({
     {
       uResolution: [window.innerWidth * 0.8, window.innerHeight * 0.8],
       uTime: 0,
-      uColor1: new Color(darkColors[1]),
-      uColor2: new Color(darkColors[2]),
-      uColor3: new Color(darkColors[3]),
-      uColor4: new Color(darkColors[4]),
+      uColor1: new Color(darkColors[1]).multiplyScalar(0.8),
+      uColor2: new Color(darkColors[2]).multiplyScalar(0.8),
+      uColor3: new Color(darkColors[3]).multiplyScalar(0.8),
+      uColor4: new Color(darkColors[4]).multiplyScalar(0.8),
     },
     vertexShader,
-    fragmentShader
+    fragmentShader2
   ),
 });
 
 function DonutsBackgroundMaterial({ theme }) {
   const ref = useRef();
-  useFrame((state, delta) => { ref.current.uTime += delta * 10; });
-  return theme === 'dark' ? <darkShaderMaterial ref={ref} /> : <lightShaderMaterial ref={ref} />;
+  useFrame((state, delta) => {
+    ref.current.uTime += delta * 10;
+  });
+  return isDarkTheme(theme) ? <darkShaderMaterial ref={ref} /> : <lightShaderMaterial ref={ref} />;
 }
 
 export default function DonutsBackground({ depth, theme }) {
@@ -56,7 +59,7 @@ export default function DonutsBackground({ depth, theme }) {
         <planeGeometry />
         <DonutsBackgroundMaterial theme={theme} />
       </mesh>
-      {theme === 'dark' && (
+      {isDarkTheme(theme) && (
         <mesh scale={[viewport.width * 10, viewport.height * 10, 1]} position={[0, 0, -depth + 1]}>
           <planeGeometry />
           <meshBasicMaterial color='black' transparent opacity={0.25} />

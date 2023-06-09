2776ccb1bd236f6962cb58dce41b1fe93405ff35
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-10 02:08:27 +0300

    add donutbg to index

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,9 @@
 import Donut from './Donut';
+import DonutsBackground from './DonutsBackground';
 import DonutsCanvas from './DonutsCanvas';
 import DonutSketch from './DonutSketch';
 import Overlay from './Overlay';
 import Preloader from './Preloader';
 import ThemeButton from './ThemeButton';
 
-export { Donut, DonutsCanvas, DonutSketch, Overlay, Preloader, ThemeButton };
+export { Donut, DonutsBackground, DonutsCanvas, DonutSketch, Overlay, Preloader, ThemeButton };
